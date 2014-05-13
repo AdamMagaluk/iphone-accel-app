4f2e6a09f625f39a75857add2fce5b95898d4393
Adam Magaluk <AdamMagaluK@gmail.com>
2014-05-13 13:13:05 -0400

    Updated to app and device name

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var static = require('node-static');
 var file = new static.Server('./public');
 
 require('http').createServer(function (request, response) {
-  request.addListener('end', function () {    
+  var clientIp = request.headers['X-Forwarded-For'] || request.connection.remoteAddress;
+  request.addListener('end', function () {
+    response.setHeader('Set-Cookie','deviceName=phone-'+clientIp);
     file.serve(request, response);
   }).resume();
 }).listen(process.env.PORT || 8080);
